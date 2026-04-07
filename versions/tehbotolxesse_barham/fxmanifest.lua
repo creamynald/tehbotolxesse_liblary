c4f90038c4bad3cc4f4e9579413cc9f5ae765295
creamynald <creamynald@gmail.com>
2026-04-07 20:37:05 +0000

    Auto-update versi tehbotolxesse_barham

---
+++ versions/tehbotolxesse_barham/fxmanifest.lua
@@ -0,0 +1,39 @@
+fx_version 'cerulean'
+game 'gta5'
+
+lua54 'yes'
+
+author 'TehBotolxESSE'
+description 'kepo bangett bwanggg'
+version '1.2.0'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'shared/config_barham.lua',
+    'shared/locale.lua',   -- safe locale fallback (runs after config so Config.Locale is set)
+}
+
+client_scripts {
+    'client/cl_barham.lua',
+    'client/cl_command.lua',
+}
+
+server_scripts {
+    'server/sv_barham.lua',
+    'server/sv_command.lua',
+}
+
+files {
+    'locales/*.json',
+    'stream/**/*.ytyp',
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/**/*.ytyp'
+
+dependencies {
+    'qbx_core',
+    'ox_lib',
+    'ox_inventory',
+    'ox_target',
+    -- 'tk_dispatch',
+}
